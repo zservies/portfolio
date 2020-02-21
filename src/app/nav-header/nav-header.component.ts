268eeb6f30f06f015e920c3d06f3fdff920bac33
Zachary Servies <zachservies@gmail.com>
2020-02-20 21:57:39 -0500

    Added ability to toggle navigation buttons in header.

--- src/app/nav-header/nav-header.component.ts
+++ src/app/nav-header/nav-header.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavHeaderComponent implements OnInit {
 
+  // Keep track of user's current page for button toggles.
+  buttonToggled =
+    {
+      home: {
+        isToggled: false
+      },
+      about: {
+        isToggled: false
+      },
+      resume: {
+        isToggled: false
+      },
+      socials: {
+        isToggled: false
+      }
+    };
+
+
   constructor() { }
 
+  toggleNav(event) {
+    // Loop through the button toggles and set them all to false
+    for (const button of Object.keys(this.buttonToggled)) {
+      this.buttonToggled[button].isToggled = false;
+    }
+    // Set the toggle emitted to true.
+    this.buttonToggled[event].isToggled = true;
+  }
+
   ngOnInit() {
+    this.buttonToggled.home.isToggled = true;
   }
 
 }
