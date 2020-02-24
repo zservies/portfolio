b2685c9baaaa4dd6fab70cbcd506c530899b1572
Zachary Servies <Zachary.Servies@karauctionservices.com>
2020-02-24 16:09:22 -0500

    Added logic to check for manually entered URLs and highlight appropriate navigation button based on pathname. Generated 'about' page component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from '../app/landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
+  {path: 'home', component: LandingPageComponent},
+  {path: 'about', component: AboutComponent},
   {path: '**', component: LandingPageComponent}
 ];
 
