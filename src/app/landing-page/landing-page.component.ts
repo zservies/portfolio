c2909e2b099deff91df0422e8a21a5ab64fee960
Zachary Servies <zachservies@gmail.com>
2020-12-16 16:30:52 -0500

    Cleaned up unused code and css in the landing page.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,29 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  constructor() {}
 
-  carouselCount = 1;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  carouselNav(direction) {
-    switch (direction) {
-      case 'next':
-        this.carouselCount++;
-        break;
-      case 'back':
-        this.carouselCount--;
-    }
-    if (this.carouselCount > 4) {
-      this.carouselCount = 1;
-    } else if (this.carouselCount === 0) {
-      this.carouselCount = 4;
-    }
-  }
+  ngOnInit() {}
 }
